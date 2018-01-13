9213b4106851319735492861f5a61687298ce303
duruo <duruo@126.com>
2018-01-13 12:45:19 +0800

    fix

--- database/migrations/2018_01_13_022048_create_shops_table.php
+++ database/migrations/2018_01_13_022048_create_shops_table.php
@@ -19,18 +19,18 @@ class CreateShopsTable extends Migration
             $table->string('icon')->comment('店铺图标');
             $table->string('cover')->comment('店铺背景大图');
             $table->string('summary')->comment('店铺简介');
-            $table->unsignedTinyInteger('type')->comment('店铺类型 个人|企业');
-            $table->unsignedTinyInteger('level')->comment('店铺等级');
-            $table->unsignedMediumInteger('province_id')->comment('店铺省份');
-            $table->unsignedMediumInteger('city_id')->comment('店铺市区');
+            $table->tinyInteger('type')->default(1)->comment('店铺类型 个人|企业');
+            $table->tinyInteger('level')->default(0)->comment('店铺等级');
+            $table->mediumInteger('province_id')->nullable()->comment('店铺省份');
+            $table->mediumInteger('city_id')->nullable()->comment('店铺市区');
             $table->string('address')->comment('店铺地址');
             $table->string('phone', 11)->comment('店铺联系电话');
             $table->string('business_licence')->comment('营业执照');
             $table->string('food_licence')->comment('食品经营许可证');
-            $table->unsignedTinyInteger('status')->comment('店铺状态');
-            $table->dateTime('requested_at')->comment('店铺申请时间');
-            $table->dateTime('opened_at')->comment('店铺开店时间');
-            $table->dateTime('closed_at')->comment('店铺关闭时间');
+            $table->tinyInteger('status')->comment('店铺状态');
+            $table->dateTime('requested_at')->nullable()->comment('店铺申请时间');
+            $table->dateTime('opened_at')->nullable()->comment('店铺开店时间');
+            $table->dateTime('closed_at')->nullable()->comment('店铺关闭时间');
             $table->timestamps();
         });
     }
