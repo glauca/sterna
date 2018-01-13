fe7f7f255bf65b31e4f51ebcb70a727e63910849
duruo <duruo@126.com>
2018-01-13 12:01:51 +0800

    model-shop

---
+++ database/migrations/2018_01_13_022048_create_shops_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->comment('店铺名称');
+            $table->string('icon')->comment('店铺图标');
+            $table->string('cover')->comment('店铺背景大图');
+            $table->string('summary')->comment('店铺简介');
+            $table->unsignedTinyInteger('type')->comment('店铺类型 个人|企业');
+            $table->unsignedTinyInteger('level')->comment('店铺等级');
+            $table->unsignedMediumInteger('province_id')->comment('店铺省份');
+            $table->unsignedMediumInteger('city_id')->comment('店铺市区');
+            $table->string('address')->comment('店铺地址');
+            $table->string('business_licence')->comment('营业执照');
+            $table->string('food_licence')->comment('食品经营许可证');
+            $table->unsignedTinyInteger('status')->comment('店铺状态');
+            $table->dateTime('requested_at')->comment('店铺申请时间');
+            $table->dateTime('opened_at')->comment('店铺开店时间');
+            $table->dateTime('closed_at')->comment('店铺关闭时间');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
