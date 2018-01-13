4e187cf6f61309eba27649070399ecc3a33e72fa
duruo <duruo@126.com>
2018-01-13 12:45:35 +0800

    model-user

--- database/migrations/2018_01_13_123426_create_user_wechats_table.php
+++ database/migrations/2018_01_13_123426_create_user_wechats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserWechatsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('nickname');
-            $table->string('mobile')->unique();
-            $table->string('password');
+        Schema::create('user_wechats', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('openid')->unique();
             $table->string('avatar');
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->primary('openid');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_wechats');
     }
 }
