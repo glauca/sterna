d4d70e619f57dc962c4533433ef8043e4c5637e4
duruo <duruo@126.com>
2018-03-13 11:00:20 +0800

    item-show

---
+++ app/Http/Controllers/H5/ItemController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\H5;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return view('h5.item.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('h5.item.show');
+    }
+}
